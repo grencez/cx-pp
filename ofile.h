fbd71897f45ab4b09b2d1126046c775bc6b52b8f
Alex Klinkhamer <grencez@codelove.org>
2013-08-01 00:04:47 -0400

    Update cx/ file functions

---
+++ ofile.h
@@ -0,0 +1,131 @@
+/*
+ * \file ofile.h
+ */
+#ifndef OFile_H_
+#define OFile_H_
+#include "alphatab.h"
+#include <stdarg.h>
+
+typedef struct OFile OFile;
+typedef struct OFileCtx OFileCtx;
+typedef struct OFileVT OFileVT;
+
+struct OFile
+{
+  TableT(byte) buf;
+  ujint off;
+  ujint flushsz;
+  OFileCtx* ctx;
+};
+
+struct OFileCtx
+{
+  const OFileVT* vt;
+};
+
+struct OFileVT
+{
+  bool (*flush_fn) (OFile*);
+  void (*free_fn) (OFile*);
+  void (*close_fn) (OFile*);
+};
+
+void
+close_OFile (OFile* of);
+void
+lose_OFile (OFile* of);
+void
+free_OFile (OFile* of);
+void
+flush_OFile (OFile* of);
+
+void
+oput_int_OFile (OFile* of, int x);
+void
+oput_uint_OFile (OFile* of, uint x);
+void
+oput_ujint_OFile (OFile* of, ujint x);
+void
+oput_real_OFile (OFile* of, real x);
+void
+oput_char_OFile (OFile* of, char c);
+void
+oput_AlphaTab (OFile* of, const AlphaTab* t);
+void
+vprintf_OFile (OFile* of, const char* fmt, va_list args);
+void
+printf_OFile (OFile* of, const char* fmt, ...);
+void
+oputn_char_OFile (OFile* of, const char* a, ujint n);
+
+/* Implemented in syscx.c */
+OFile* stdout_OFile ();
+OFile* stderr_OFile ();
+
+qual_inline
+  void
+init_OFile (OFile* of)
+{
+  static char empty[1] = { 0 };
+  InitTable( of->buf );
+  of->buf.s = (byte*) empty;
+  of->buf.sz = 1;
+  of->off = 0;
+  of->flushsz = 0;
+  of->ctx = 0;
+}
+
+qual_inline
+  void
+mayflush_OFile (OFile* of)
+{
+  if (of->flushsz > 0 && of->off > of->flushsz)
+    of->ctx->vt->flush_fn (of);
+}
+
+qual_inline
+  void
+oput_cstr_OFile (OFile* of, const char* s)
+{
+  const AlphaTab t = dflt1_AlphaTab (s);
+  oput_AlphaTab (of, &t);
+}
+
+qual_inline
+  char*
+cstr1_OFile (OFile* f, ujint off)
+{ return (char*) &f->buf.s[off]; }
+
+qual_inline
+  char*
+cstr_OFile (OFile* of)
+{ return cstr1_OFile (of, of->off); }
+
+qual_inline
+  AlphaTab
+AlphaTab_OFile (OFile* of, ujint off)
+{
+  DeclTable( char, t );
+  t.s = (char*) &of->buf.s[off];
+  t.sz = (of->off - off) / sizeof(char);
+  return t;
+}
+
+qual_inline
+  void
+cat_AlphaTab_OFile (AlphaTab* t, OFile* of)
+{
+  AlphaTab tmp = AlphaTab_OFile (of, 0);
+  cat_AlphaTab (t, &tmp);
+}
+
+qual_inline
+  void
+copy_AlphaTab_OFile (AlphaTab* t, OFile* of)
+{
+  AlphaTab tmp = AlphaTab_OFile (of, 0);
+  copy_AlphaTab (t, &tmp);
+}
+
+#endif
+
---
+++ xfile.h
@@ -0,0 +1,132 @@
+/*
+ * \file xfile.h
+ */
+#ifndef XFile_H_
+#define XFile_H_
+#include "alphatab.h"
+
+typedef struct XFile XFile;
+typedef struct XFileCtx XFileCtx;
+typedef struct XFileVT XFileVT;
+
+struct XFile
+{
+  TableT(byte) buf;
+  ujint off;
+  ujint flushsz;
+  XFileCtx* ctx;
+};
+
+struct XFileCtx
+{
+  const XFileVT* vt;
+};
+
+
+struct XFileVT
+{
+  bool (*xget_chunk_fn) (XFile*);
+  
+
+  void (*free_fn) (XFile*);
+  void (*close_fn) (XFile*);
+};
+
+void
+close_XFile (XFile* xf);
+void
+lose_XFile (XFile* xf);
+void
+free_XFile (XFile* xf);
+void
+flush_XFile (XFile* f);
+
+void
+xget_XFile (XFile* xf);
+char*
+getline_XFile (XFile* in);
+char*
+getlined_XFile (XFile* xf, const char* delim);
+void
+skipds_XFile (XFile* xf, const char* delims);
+char*
+nextds_XFile (XFile* in, char* ret_match, const char* delims);
+char*
+tods_XFile (XFile* xf, const char* delims);
+char*
+nextok_XFile (XFile* xf, char* ret_match, const char* delims);
+void
+inject_XFile (XFile* in, XFile* src, const char* delim);
+void
+skipto_XFile (XFile* xf, const char* pos);
+
+bool
+xget_int_XFile (XFile* xf, int*);
+bool
+xget_uint_XFile (XFile* xf, uint*);
+bool
+xget_ujint_XFile (XFile* xf, ujint*);
+bool
+xget_real_XFile (XFile* xf, real*);
+bool
+xget_char_XFile (XFile* xf, char*);
+
+/* Implemented in syscx.c */
+XFile* stdin_XFile ();
+
+qual_inline
+  void
+init_XFile (XFile* xf)
+{
+  static char empty[1] = { 0 };
+  InitTable( xf->buf );
+  xf->buf.s = (byte*) empty;
+  xf->buf.sz = 1;
+  xf->off = 0;
+  xf->flushsz = 0;
+  xf->ctx = 0;
+}
+
+qual_inline
+  void
+olay_XFile (XFile* olay, XFile* xf, uint off)
+{
+  init_XFile (olay);
+  olay->buf.s = &xf->buf.s[off];
+  olay->buf.sz = xf->off - off;
+}
+
+qual_inline
+  void
+mayflush_XFile (XFile* xf, Trit may)
+{
+  if (may == Yes)  xf->flushsz = 1;
+
+  if (xf->flushsz > 0 && xf->off > CeilQuot( xf->buf.sz, 4 ))
+    flush_XFile (xf);
+
+  if (may == Nil)  xf->flushsz = 0;
+}
+
+qual_inline
+  char*
+cstr1_XFile (XFile* f, ujint off)
+{ return (char*) &f->buf.s[off]; }
+
+qual_inline
+  char*
+cstr_XFile (XFile* xf)
+{ return cstr1_XFile (xf, xf->off); }
+
+qual_inline
+  AlphaTab
+AlphaTab_XFile (XFile* xf, ujint off)
+{
+  DeclTable( char, t );
+  t.s = (char*) &xf->buf.s[off];
+  t.sz = (xf->off - off) / sizeof(char);
+  return t;
+}
+
+#endif
+
